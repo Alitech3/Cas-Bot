23bd4c31be00e8518b4f866ebfe4311298b5781e
Ali <Alitech3@users.noreply.github.com>
2018-09-23 03:00:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js');
+
 const bot = new Discord.Client();
 
 const Settings = require('./Settings.json');
+
+process.on('unhandledRejection', error => console.error(`Uncaught Promise Rejection:\n${error}`));
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
+  if(msg.author.bot) {
+    return;
+  }
+  if(!msg.content.startsWith(Settings.Prefix)){
+    return;
+  }
+  const args = msg.content.split(Settings.Prefix);
+  if (args[1] === 'ping') {
     msg.reply('Pong!');
   }
 });
