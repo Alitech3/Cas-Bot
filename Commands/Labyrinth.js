dd4fedc52ad94ceb3941158ea96682070a8ed24a
Ali <Alitech3@users.noreply.github.com>
2018-12-22 02:15:20 -0500

    Add files via upload

--- Commands/Labyrinth.js
+++ Commands/Labyrinth.js
@@ -1,6 +1,6 @@
 // exports the command and runs it for when its called in the index.js
 // we are also taking the parameters args and msg from index.js
-exports.run = async function(args, msg) {
+exports.run = async function(args, msg, Discord) {
 
 	// escape the laybrinth mini game
 
@@ -32,22 +32,37 @@ exports.run = async function(args, msg) {
 	// this sends the correct path (this will be removed when the command is functional)
 	msg.channel.send([CP]);
 
-	// everything below here does not work disregard
-	const filter = m => m.content.includes([CP]);
+	// everything below here works depending on how you look at it
 
-	await msg.channel.awaitMessages(filter,
-		{ max: 4, time: 10000, errors: ['time'] })
-		.then(collected => console.log(`${collected.size} values`))
-		.catch(collected => console.log(`After a minute, only ${collected.values()} out of 4 voted.`));
-	console.log(CP);
-	console.log(msg.content);
-	msg.channel.send(filter.map(m => m.content));
+	let k = 0;
 
+	// working depending on your definition of working
+	msg.channel.send('Welcome to the labyrinth; your goal - escape. You can go left, right, or straight\nyou have 10 seconds to decide before you are lost to the abyss\nwhich way do you go?')
+		.then(() => {
+			const collector = new Discord.MessageCollector(msg.channel, mg => mg.author.id === msg.author.id, { time: 10000 });
+			collector.on('collect', ms => {
+				if (ms.content == CP[k]) {
+					ms.channel.send('Well done you have made it farther inside which direction is next?');
+					k++;
+				}
+				else if(ms.content == CP[k] && k == 9) {
+					ms.channel.send('You have succesfully escaped the labyrinth');
+				}
+				else{
+					return ms.channel.send('You fell in a pit and died. You have been lost to the labyrinth');
+				}
+			});
+		});
+
+	// if first one is correct continue if wrong stop
+	// use end event
 	/*
 		Command side notes:
 		# need to add a possiblity of dead ends for the labyrinth
 		# need to change CP to where it is more dynamic (so user can pick difficulty rather than only 10)
 		# timer for one user to answer till it says "you have been lost to the labyrinth";
+		# win conditon
+		# lose condition
 	*/
 
 };
